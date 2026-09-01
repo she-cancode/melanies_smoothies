7e02faa706ac5f608338c6daa86ffafa9aa0b59d
Terinique <briannaterikok@gmail.com>
2026-09-01 13:47:17 +0200

    Add requests import and fetch smoothie data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 from snowflake.snowpark.functions import col
 import streamlit as st
+import requests  
 
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
@@ -25,7 +26,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+        
     # Updated column list to include name_on_order
     my_insert_stmt = f"""insert into smoothies.public.orders(ingredients, name_on_order)
             values ('{ingredients_string.strip()}', '{name_on_order}')"""
@@ -36,7 +39,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
         
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
+
+
